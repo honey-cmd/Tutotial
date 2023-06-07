1d0f21a327e651086448512737d72e5e4b3afccf
hanni <pyaesone.hanni@gmail.com>
2023-06-07 19:44:13 +0630

    numbergenerate

---
+++ src/test_tuto/NumberGenerate.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package test_tuto;
+
+public class NumberGenerate {
+
+
+	public static void main(String[] args) {
+
+		int i, j, k, n = 7;
+
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j < i; j++) {
+				System.out.print(' ');
+			}
+
+			for (k = i; k <= n; k++) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+		for (i = n - 1; i >= 1; i--) {
+			for (j = 1; j < i; j++) {
+				System.out.print(' ');
+			}
+			for (k = i; k <= n; k++) {
+				System.out.print(k + " ");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
